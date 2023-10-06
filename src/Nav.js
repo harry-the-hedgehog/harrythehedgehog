787b169b4281a662a61f66ccd3aa7990b2bf1e1f
HarryTheHedgehog <yyy161073@gmail.com>
2023-10-06 18:40:42 +0100

    hi

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <div id="logo"></div>
+      <div id="home" className="navItem theme"></div>
+      <div id="product" className="navItem theme"></div>
+      <div id="news" className="navItem theme"></div>
+      <div id="support" className="navItem theme"></div>
+      <div id="contact" className="navItem theme"></div>
+    </nav>
+  );
+}
